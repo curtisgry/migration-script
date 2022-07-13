63f529be0248845ec82c9247ce171cfa6f245641
Curtis <curtis@Curtiss-MacBook-Pro.local>
2022-07-13 13:54:20 -0400

    initial date filter working

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// const prompt = require("prompt-sync")({ sigint: true });
+const prompt = require("prompt-sync")({ sigint: true });
 const fs = require("fs");
 require("dotenv").config();
 const axios = require("axios");
 const colors = require("colors");
+const validate = require("./lib/inputValidation")
 
 const simpleGit = require("simple-git");
 simpleGit().clean(simpleGit.CleanOptions.FORCE);
@@ -17,7 +18,7 @@ const octokit = new Octokit({ auth: process.env.GH_ACCESS_TOKEN });
 // const password = prompt('Enter your app password: ')
 
 // Variables for testing
-const workspace = process.env.USERNAME;
+const workspace = process.env.WORKSPACE;
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 const ghUsername = process.env.GH_USERNAME;
@@ -27,30 +28,69 @@ const repoDataList = [];
 
 // Returns all data for repos from the api
 const getAllRepoData = async () => {
-  const res = await axios.get(
-    `https://api.bitbucket.org/2.0/repositories/${workspace}`,
-    {
-      auth: {
-        username,
-        password,
-      },
-    }
-  );
-  return res.data;
+  try {
+    const res = await axios.get(
+      `https://api.bitbucket.org/2.0/repositories/${workspace}`,
+      {
+        auth: {
+          username,
+          password,
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+// Date format YYYY-MM-dd
+// Operators = != > >= < <=
+const getReposByDate = async ({ operator, date }) => {
+  try {
+    const res = await axios.get(
+      `https://api.bitbucket.org/2.0/repositories/${workspace}?&q=updated_on${operator}${date}`,
+      {
+        auth: {
+          username,
+          password,
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    console.log(`Status: ${error.response.status}`.yellow);
+    console.log(`${error.response.statusText}`.red);
+    return null;
+  }
 };
 
-//Populate repoDataList with slug and clone url
-const makeRepoDataList = async () => {
-  const data = await getAllRepoData();
-  //Pulling out just the slug and clone url from the api response array
+//Pull data needed from api response data
+const extractDataToArray = (data, arr) => {
+  if (!data) return console.log("No repos found with search.");
+  let id = 0;
   data.values.forEach((val) => {
     const repoInfo = {
       slug: val.slug,
       clone: val.links.clone[0].href,
+      dateUpdated: val.updated_on,
+      id,
     };
+    id++;
     //currently pushing into global const repoDataList
-    repoDataList.push(repoInfo);
+    arr.push(repoInfo);
   });
+};
+
+//Populate repoDataList with slug and clone url
+const makeRepoDataList = async (filter, filterOptions) => {
+  let data;
+  if (filter && filterOptions) {
+    data = await getReposByDate(filterOptions);
+  } else {
+    data = await getAllRepoData();
+  }
+  extractDataToArray(data, repoDataList);
 };
 
 // Clone repo from bitbucket
@@ -72,7 +112,7 @@ const createGitHubRepo = async (repoName) => {
 };
 
 async function pushToGithub(gitName, remoteUrl) {
-    // workingDir to set path for simpleGit
+  // workingDir to set path for simpleGit
   const workingDir = __dirname + `/${gitName}.git`;
   try {
     console.log(`Removing remote origin...`.bgGreen);
@@ -89,6 +129,69 @@ async function pushToGithub(gitName, remoteUrl) {
   }
 }
 
+const checkOperatorInput = (op) => {
+  //Regex for comparison operators
+  const regex = /^(\!=|=|>|<|>=|<=)$/;
+  if (!op.match(regex)) return false;
+  return true;
+};
+
+const checkDateInput = (date) => {
+  //Regex for date format
+  const regex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
+  if (!date.match(regex)) return false;
+  return true;
+};
+
+const checkInputBool = (letter) => {
+    const regex = /^(y|n)$/;
+  if (!letter.match(regex)) return false;
+  return true;
+}
+
+const operatorInput = (operatorVar) => {
+  const operator = prompt("Enter an operator[ = , != , >  , >=  , <= ] : ");
+
+  if (!validate.operator(operator)) {
+    console.log("Invalid operator".red);
+    operatorInput(operatorVar);
+  }
+  operatorVar.operator = operator;
+};
+
+const dateInput = (dateVar) => {
+  const dateVal = prompt("Enter a date YYYY-MM-dd : ");
+  if (!validate.date(dateVal)) {
+    console.log("Invalid date".red);
+    dateInput(dateVar);
+  }
+  dateVar.date = dateVal;
+};
+
+const inputLoop = async () => {
+  //for testing
+  const filterOptions = {};
+  const isFilteredList = prompt("Would you like to apply a filter by date?(y/n)")
+
+
+  if (isFilteredList) {
+    operatorInput(filterOptions);
+    dateInput(filterOptions);
+  }
+  console.log(filterOptions);
+
+  //get repo data into array
+
+  await makeRepoDataList(isFilteredList, filterOptions);
+  // console.log(repoDataList)
+  if (!repoDataList.length) {
+    console.log("No results".bgCyan);
+    inputLoop();
+  } else {
+    return;
+  }
+};
+
 (async () => {
   // Authenticate octokit
   const {
@@ -96,29 +199,27 @@ async function pushToGithub(gitName, remoteUrl) {
   } = await octokit.rest.users.getAuthenticated();
   console.log("Hello, %s", login);
 
-  //for testing
+  await inputLoop();
 
-  //get repo data into array
-  await makeRepoDataList();
-  // console.log(repoDataList)
-  const { clone, slug } = repoDataList[1];
+  const { clone, slug } = repoDataList[0];
   // create github remote repo
-  await createGitHubRepo(slug);
+  //   await createGitHubRepo(slug);
   const remoteUrl = `https://github.com/curtisgry/${slug}`;
   // clone repo from bitbucket
-  await cloneRepo(clone);
+  //   await cloneRepo(clone);
   //for deleting folder later
   const repoPath = __dirname + `/${slug}.git`;
   //repo name and remote url
-  await pushToGithub(slug, remoteUrl);
+  //   await pushToGithub(slug, remoteUrl);
 
   // Cleanup! delete directory
-  await fs.rmdir(repoPath, { recursive: true }, (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log(`Removing ${slug} directory...`);
-  });
+  //   await fs.rmdir(repoPath, { recursive: true }, (err) => {
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     console.log(`Removing ${slug} directory...`);
+  //   });
 
-  console.log(`Finished! Here is the new GitHub repo: ${remoteUrl}`.green);
+  //   console.log(`Finished! Here is the new GitHub repo: ${remoteUrl}`.green);
+  console.log(repoDataList);
 })();
